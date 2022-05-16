3670b7529bd1b69d148ada2b738e0f4b394e5541
user <tfh100@gmail.com>
2022-05-17 00:33:03 +0900

    객체 큐, 스택

---
+++ dataStructure/test.js
@@ -0,0 +1,28 @@
+import { Queue } from "./queue.js";
+import Stack from "./stack.js";
+
+// const stack = new Stack();
+// console.log(stack);
+// stack.peek();
+// stack.push("abc");
+// stack.push("abcd");
+// stack.push("abcde");
+// console.log(stack);
+// console.log(stack.top);
+// stack.pop();
+// stack.pop();
+// stack.pop();
+
+// const queue = new Queue();
+
+// queue.add("hello1");
+// queue.add("hello2");
+// queue.add("hello3");
+// queue.add("hello4");
+// queue.add("hello5");
+// queue.add("hello6");
+
+// console.log(queue);
+// console.log(queue.size());
+
+const 
